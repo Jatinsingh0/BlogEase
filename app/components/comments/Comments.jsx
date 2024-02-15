346df375bb2c41020c193682958f7112ccf5bdac
Jatin singh <jatinsingh10001@gmail.com>
2024-02-15 13:17:20 +0530

    changes in write page

--- app/components/comments/Comments.jsx
+++ app/components/comments/Comments.jsx
@@ -30,6 +30,7 @@ const Comments = ({postSlug}) => {
       method: "POST",
       body: JSON.stringify({desc, postSlug}) 
     });
+    setDesc("");
     mutate();
   };
   
--- app/write/page.jsx
+++ app/write/page.jsx
@@ -15,7 +15,7 @@ import dynamic from "next/dynamic";
 
 const WritePage = () => {
   const router = useRouter();
-  const ReactQuill = dynamic(() => import('react-quill'), {ssr: false});
+  const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
   const [open, setOpen] = useState(false);
   const [file, setFile] = useState(null);
@@ -127,7 +127,7 @@ const WritePage = () => {
           className={styles.textArea}
           theme="bubble"
           value={value}
-          onChange={setValue}
+          onChange={(content, _, source, editor) => setValue(editor.getHTML())}
           placeholder="Tell your story..."
         />
       </div>
