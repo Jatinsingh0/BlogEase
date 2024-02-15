8ae10f523f5b0aee893130b1fc128b4c02ea0650
Jatin singh <jatinsingh10001@gmail.com>
2024-02-15 23:48:47 +0530

    ,,

--- app/write/page.jsx
+++ app/write/page.jsx
@@ -86,7 +86,7 @@ const WritePage = () => {
         desc: value,
         img: media,
         slug: slugify(title),
-        catSlug: catSlug || "style", //If not selected, choose the general category
+        catSlug: catSlug 
       }),
     });
 
